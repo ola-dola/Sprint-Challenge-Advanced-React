f03c5602c0e24653af1b6cbf9412a61ad6621d53
oredola <oredolaolamide@gmail.com>
2019-12-06 11:10:43 +0100

    Custom hook used to fetch data

--- client/src/Components/Players.js
+++ client/src/Components/Players.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {useFetch} from '../hooks/index'
 import PlayerCard from "./PlayerCard"
 
 export default function Players({players}) {
 
+  const [img] = useFetch("https://api.thecatapi.com/v1/images/search");
+  // console.log(img);
+  
   return (
     <div>
+      <h1>Lorem ipsummm</h1>
+      <img src={img} alt="randoms!!" />
       {
         players.map(player => {
           return <PlayerCard player={player} />
