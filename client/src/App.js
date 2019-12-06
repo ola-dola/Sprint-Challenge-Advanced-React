2339655894a2f5df4b90508fd6ad3aacb33c54e8
oredola <oredolaolamide@gmail.com>
2019-12-06 10:51:55 +0100

    Fetching data from API. Added custom hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      players: ""
+    }
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/players")
+      .then(response => {
+        this.setState({
+          players: response.data
+        });
+        console.log(this.state);
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+  }
+
+  render() { 
+    return (
+      <div className="App">
+        pol
+      </div>
+    );
+  }
 }
 
 export default App;
